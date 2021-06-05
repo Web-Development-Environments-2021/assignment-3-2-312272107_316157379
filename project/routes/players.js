e974a8e1a57dc7462219e5ed2edb196d0cd171b7
tomRob123 <59135978+tomRob123@users.noreply.github.com>
2021-06-06 00:57:47 +0300

    api completed before final changes

--- project/routes/players.js
+++ project/routes/players.js
@@ -1,18 +1,23 @@
 var express = require("express");
 var router = express.Router();
-const users_utils = require("./utils/users_utils");
 const players_utils = require("./utils/players_utils");
-const LEAGUE_ID = 271;
+const fs = require("fs");
+const logStream = fs.createWriteStream("log.txt", { flags: "a" });
 
 router.get("/:player_id", async (req, res, next) => {
-    try {
-        const player_info = players_utils.get_info(req.params.player_id,'get_player_by_id');
-        res.status(200).send(player_info);
-        logStream.end(`player information successfully retrieved when searching for ${player_id}`);
-    } catch (error) {
-      logStream.end(error.message);
-      next(error);
-    }
-  });
+  try {
+    const player_info = await players_utils.get_info([req.params.player_id]);
+    res.status(200).send(player_info);
+    logStream.end(
+      `player information successfully retrieved when searching for ${req.params.player_id}`
+    );
+  } catch (error) {
+    logStream.end(error.message);
+    next({
+      status: 404,
+      message: "invalid player id",
+    });
+  }
+});
 
 module.exports = router;
